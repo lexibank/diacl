20838fdb670e1d8f4598e9cbaa50793d9a73e328
chrzyki <christoph@functor-argument.net>
2019-11-06 12:29:33 +0100

    Updated setup

--- setup.py
+++ setup.py
@@ -2,29 +2,19 @@ from setuptools import setup
 import json
 
 
-with open('metadata.json') as fp:
+with open("metadata.json") as fp:
     metadata = json.load(fp)
 
 
 setup(
-    name='lexibank_diacl',
-    description=metadata['title'],
-    license=metadata.get('license', ''),
-    url=metadata.get('url', ''),
-    py_modules=['lexibank_diacl'],
+    name="lexibank_diacl",
+    description=metadata["title"],
+    license=metadata.get("license", ""),
+    url=metadata.get("url", ""),
+    py_modules=["lexibank_diacl"],
     include_package_data=True,
     zip_safe=False,
-    entry_points={
-        'lexibank.dataset': [
-            'diacl=lexibank_diacl:Dataset',
-        ]
-    },
-    install_requires=[
-        'pylexibank>=2.0',
-    ],
-    extras_require={
-        'test': [
-            'pytest-cldf',
-        ],
-    },
+    entry_points={"lexibank.dataset": ["diacl=lexibank_diacl:Dataset"]},
+    install_requires=["pylexibank>=2.1"],
+    extras_require={"test": ["pytest-cldf"]},
 )
