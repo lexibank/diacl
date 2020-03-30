1f991aacf4c9de34f3d0dbfcb0dc65d595a2f4cb
chrzyki <christoph@functor-argument.net>
2020-03-30 10:50:50 +0200

    Fix test

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ def test_valid(cldf_dataset, cldf_logger):
 
 
 def test_forms(cldf_dataset):
-    assert len(list(cldf_dataset["FormTable"])) == 60183
+    assert len(list(cldf_dataset["FormTable"])) == 60138
     assert any(f["Form"] == "cǝ´ǝrai" for f in cldf_dataset["FormTable"])
 
 
